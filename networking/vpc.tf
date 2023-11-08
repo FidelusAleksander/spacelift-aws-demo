5fde7910f59f9b0e59598dbb6e7eb632f224e38d
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2023-11-08 12:25:22 +0100

    Delete private subnet

--- networking/vpc.tf
+++ networking/vpc.tf
@@ -14,12 +14,3 @@ resource "aws_subnet" "public" {
     Name = "spacelift-public-subnet"
   }
 }
-
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "spacelift-private-subnet"
-  }
-}
