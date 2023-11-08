48874adef82ec6f4bb5cc8e9c5ba4142b42f1c68
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2023-11-08 11:12:08 +0100

    add networking terraform code

---
+++ networking/main.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "example" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "spacelift-vpc"
+    }
+}
+
+resource "aws_subnet" "public" {
+    vpc_id     = aws_vpc.example.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+        Name = "spacelift-public-subnet"
+    }
+}
+
+resource "aws_subnet" "private" {
+    vpc_id     = aws_vpc.example.id
+    cidr_block = "10.0.2.0/24"
+
+    tags = {
+        Name = "spacelift-private-subnet"
+    }
+}
+
+output "vpc_id" {
+    value = aws_vpc.example.id
+}
