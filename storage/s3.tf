5580f01bc9519a2972387d82cf3002c07b26c574
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2023-11-14 18:59:00 +0100

    Add s3 public access block

--- storage/s3.tf
+++ storage/s3.tf
@@ -2,9 +2,13 @@ resource "aws_s3_bucket" "this" {
   bucket = "fidel-spacelift-demo"
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "this" {
